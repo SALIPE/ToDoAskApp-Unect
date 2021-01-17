e3aebd7c6ee8c44fbdf3642a8a01067c4c69c086
Felipe Bueno de Souza <66335158+SALIPE@users.noreply.github.com>
2021-01-17 11:39:37 -0300

    Atualizando backend #1

--- backend/src/controller/PostController.js
+++ backend/src/controller/PostController.js
@@ -1,12 +1,24 @@
 const Post = require('../models/Post');
+const path = require('path');
 
 module.exports = {
     async index(req, res) {
+        const posts = await Post.find().sort('-createdAt');
+
+        return res.json(posts);
         
     },
 
     async store(req, res){
-        return res.json();
+        //console.log(req.body);
+        const { todo } = req.body;
+        const post = await Post.create({
+            todo,
+        })
+      
+
+
+        return res.json(post);
     }
 
 };
--- backend/src/models/Post.js
+++ backend/src/models/Post.js
@@ -4,7 +4,6 @@ const PostSchema = new mongoose.Schema({
     todo: String,
 },{
     timestamps: true,
-}
-);
+});
 
 module.exports = mongoose.model('Post', PostSchema);
