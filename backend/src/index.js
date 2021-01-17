9c2a4b502b2eef35dba8bc610e408fa4384be1b3
Felipe Bueno de Souza <66335158+SALIPE@users.noreply.github.com>
2021-01-17 12:12:25 -0300

    Arrumando DB

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,7 +9,7 @@ const app = express();
 //const server = require('http').Server(app);
 //const io = require('socket.io')(server);
 //conexao com o mongodb
-mongoose.connect('mongodb+srv://dbUnectTODO:UnectTODO@cluster0.xs9pv.mongodb.net/<dbname>?retryWrites=true&w=majority',{
+mongoose.connect('mongodb+srv://dbUnectTODO:UnectTODO@cluster0.svzi1.mongodb.net/<dbname>?retryWrites=true&w=majority',{
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
@@ -23,4 +23,4 @@ mongoose.connect('mongodb+srv://dbUnectTODO:UnectTODO@cluster0.xs9pv.mongodb.net
 //app.use(cors());
 //mostrando o acesso a rotas de conexão
 app.use(require('./routes'));
-app.listen(3333);
+app.listen(3030);
