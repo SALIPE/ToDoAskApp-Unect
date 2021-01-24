dcdf0619c91703b1785b3bf3cead81c811800eb6
Felipe Bueno de Souza <66335158+SALIPE@users.noreply.github.com>
2021-01-24 18:05:41 -0300

    Comando do db

--- frontend/src/pages/ToDo.js
+++ frontend/src/pages/ToDo.js
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import api from '../service/api';
+import io from 'socket.io-client';
 import './ToDo.css';
 import addbt from '../assets/add.svg';
 import donebt from '../assets/Done.svg';
@@ -9,6 +10,7 @@ export default class ToDo extends Component {
   state={
     tarefas:[],
     feitos:[],
+    todo:'',
   };
 
  /* constructor() {
@@ -19,6 +21,8 @@ export default class ToDo extends Component {
     };*/
 
     async componentDidMount(){
+      this.registerToSocket();
+
       const response = await api.get('posts/todo');
       const responseComplete = await api.get('posts/completed');
 
@@ -26,6 +30,33 @@ export default class ToDo extends Component {
         tarefas: response.data, 
         feitos: responseComplete.data
       });
+    }
+
+    handleChange = e =>{
+      e.preventDefault();
+      const state = Object.assign({},this.state);
+      state[e.target.name] = e.target.value;
+      this.setState(state);
+    }
+
+    registerToSocket=()=>{
+      const socket =io('http://localhost:3000');
+
+      socket.on('update', updated=>{
+        this.setState({
+          tarefas: [updated, ...this.state.tarefas]
+        });
+      })
+    }
+
+    handleSubmit  = async e =>{
+  
+      const data = new FormData();
+      data.append('todo', this.state.todo);
+      await api.post('posts', data)
+
+      
+      console.log(this.state);
     }
 /*
     this.adicionarTarefa = (event) => {
@@ -52,6 +83,10 @@ export default class ToDo extends Component {
     };
   };*/
 
+  handleDone= id =>{
+    api.put(`/posts/update/${id}`);
+  }
+
   render() {
     return (
       /*<ListaView
@@ -68,8 +103,8 @@ export default class ToDo extends Component {
         <div className = "header-content">
           <h1>uTask</h1>
           <div className="add-task">
-            <input name = "inputTarefa"></input>
-            <button className="btadd"><img src={addbt} alt="btadd"/></button>
+            <input name ="todo" placeholder="Task" onChange={this.handleChange} value={this.state.todo}/>
+            <button type="submit" className="btadd" onClick={this.handleSubmit}><img src={addbt} alt="btadd"/></button>
           </div>
         </div>
       </header>
@@ -78,10 +113,10 @@ export default class ToDo extends Component {
           <h2>TODO</h2>
           <section>
           {this.state.tarefas.map(post=>(
-          <div className = "tarefas">
+          <div key={post._id} className = "tarefas">
             <p>{post.todo}</p>
             <div className = "bttask">
-              <button className = "btdone"><img src={donebt} alt="btndone" /></button>
+              <button className = "btdone" onClick={()=> this.handleDone(post._id) }><img src={donebt} alt="btndone" /></button>
               <button className = "btdelete"><img src={deletebt} alt="btdelete" /></button>
             </div>
           </div>))}
@@ -92,10 +127,9 @@ export default class ToDo extends Component {
           <h2>DONE</h2>
           <section>
           {this.state.feitos.map(post=>(
-          <div className = "tarefas">
+          <div key={post._id} className = "tarefas">
             <p>{post.todo}</p>
             <div className = "bttask">
-              <button className = "btdone"><img src={donebt} alt="btndone" /></button>
               <button className = "btdelete"><img src={deletebt} alt="btdelete" /></button>
             </div>
           </div>))}
